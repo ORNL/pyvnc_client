158e31d3d0f374ee448507f0d6b28f87216096fb
brian.weber <weberb@ornl.gov>
2021-08-09 09:00:36 -0700

    finish packaging

---
+++ upload_to_pypi.ps1
@@ -0,0 +1,3 @@
+$name=$args[0]
+scp dist/$name pypiserver@corr-boss:/tmp/.
+ssh pypiserver@corr-boss "python3 -m twine upload -u 7uw -p \`"$(cat pypi_password)\`" --repository-url http://localhost:18080 /tmp/$name" 
