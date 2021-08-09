b93613487940b48c5609c12c025d26241796e225
matthew.dixson <dixsonmk@ornl.gov>
2021-08-09 06:44:53 -0700

    Mouse events

--- vnc_testing.py
+++ vnc_testing.py
@@ -4,6 +4,7 @@ import sys
 import time
 
 from des import DesKey
+from keysym import BS, ENTER
 
 HANDSHAKE = ""
 PIXEL_FORMAT = "BBBBHHHBBBxxx"
@@ -25,12 +26,12 @@ class VClient:
     def __del__(self):
         self.s.close()
 
+    #Performs the protocol handshake
     def protocol_handshake(self):
         self.vnc_server_version = self.s.recv(12)
         self.s.send(b'RFB 003.008\n')
 
-# tight vnc sends a malformed VNC packet ?!
-
+    #Performs the security handshake
     def security_handshake(self):
         # Get security types 
         types = self.s.recv(4096)
@@ -45,6 +46,7 @@ class VClient:
         # Retrieve SecurityResult
         self.s.recv(4096)
 
+    # Solves the server's challenge
     def solve_challenge(self, challenge):
         # encrypt challenge with password
         new_password = bytearray()
@@ -61,6 +63,8 @@ class VClient:
         new_password = bytes(new_password)
         return new_password
 
+
+    # Sends ClientInit and receives ServerInit
     def initialization(self):
         # ClientInit (don't share)
         self.s.send(b'\x00')
@@ -74,36 +78,63 @@ class VClient:
         print(framebuffer_width, framebuffer_height, pixel_format, name_string)
     
   
+
+    # Press a key. This type it once
     def press_key_event(self, key):
         message = struct.unpack(STRING.format(8),
                                 b'\x04\x01\x00\x00\x00\x00' + key)[0]
-        print(message)
         self.s.send(message)
-    
-
+        
+        
+    # Release a key that has already been pressed. Typing a key
+    # without releasing it causes repeated key strokes to not
+    # work well
     def release_key_event(self, key):
-        #message = b'\x04\x00\x00\x00\x00\x00' + key
         message = struct.unpack(STRING.format(8),
                                 b'\x04\x00\x00\x00\x00\x00' + key)[0]
         self.s.send(message)
 
+    
+    # Press and release a key
+    def type_key(self, key):
+        self.press_key_event(key)
+        self.release_key_event(key)
 
+    # Types an entire ASCII string
     def write_string(self, string):
         for char in string:
             bytes_ = struct.pack(U16, ord(char))
-            self.press_key_event(bytes_)
-            self.release_key_event(bytes_)
+            self.type_key(bytes_)
+    
+    def pointer_event(self, left=False, middle=False, right=False, up=False, down=False, x=b'\x00\x00', y=b'\x00\x00'):
+        button_mask = 0x00
+        if left:
+            button_mask |= 0x01
+        
+        if middle:
+            button_mask |= 0x02
+
+        if right:
+            button_mask |= 0x04
+
+        if up:
+            button_mask |= 0x08
+
+        if down:
+            button_mask |= 0x10
+        
+        bm_bytes = struct.pack(U8, button_mask)
+        event = struct.unpack(STRING.format(6),
+                                b'\x05' + bm_bytes + x + y)[0]
+        self.s.send(event)
+        event = struct.unpack(STRING.format(6),
+                                b'\x05\x00' + x + y)[0]
+        self.s.send(event)
 
 
 client = VClient()
 client.protocol_handshake()
 client.security_handshake()
 client.initialization()
-#time.sleep(1)
-client.write_string("Hello World!")
-#client.press_key_event(b'\x00\x61')
-"""client.press_key_event(b'\x00\x62')
-time.sleep(1)
-client.release_key_event(b'\x00\x61')
-client.release_key_event(b'\x00\x62')"""
-
+time.sleep(5)
+client.pointer_event(left=True, x=b'\x00\xff', y=b'\x00\x00')
